c31cf2c17e234b9a41b08a620e15b90d75fbd6bd
Zain <zainikram704@gmail.com>
2026-06-10 01:16:14 +0500

    fix port for railway

--- server.js
+++ server.js
@@ -48,9 +48,10 @@ app.use('/api/analytics', analyticsRoutes);
 mongoose
   .connect(process.env.MONGO_URI)
   .then(() => {
+    const PORT = process.env.PORT || 8080;
     console.log('MongoDB Connected!');
-    app.listen(process.env.PORT, () => {
-      console.log(`Server running on port ${process.env.PORT}`);
+    app.listen(PORT, () => {
+      console.log(`Server running on port ${PORT}`);
     });
   })
   .catch((err) => console.error('MongoDB Error:', err));
